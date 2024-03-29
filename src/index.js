e7787e243ad7b1c09131b08a4d33758dec22f61b
Dmitry Shirkov <dimashirkov01051995@gmail.com>
2024-03-29 16:27:47 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/style.css';
+import './js/app';
+import './scss/style.scss'; // Global style
+import './scss/fonts.scss'; 
+import './scss/page-preview.scss'
+import './scss/product-page.scss'; 
+
+import './scss/main.scss';
+
+// TODO: write your code in app.js
